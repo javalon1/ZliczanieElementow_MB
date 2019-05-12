21588a39484bd60d45e4fda06368f6a1567a94af
bajserowiczMikolaj <mbajserowicz@gmail.com>
2019-05-12 15:00:57 +0100

    ze skanerkiem

--- src/main/java/ com/sda/algorytmy/App.java
+++ src/main/java/ com/sda/algorytmy/App.java
@@ -1,5 +1,7 @@
 package  com.sda.algorytmy;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -9,9 +11,16 @@ public class App
     public static void main( String[] args )
     {
 
+
+
+
         int []a = {4, 1, 6, 1, 1, 4, 3, 3, 1};
         int wynik = 0;
-        int x = 1;
+
+        System.out.println("podaj szukana liczbe:");
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+
         int n = a.length;
         for (int i =0; i<n; i++){
             if (a[i] == x){

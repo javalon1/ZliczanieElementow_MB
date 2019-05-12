4731d8e72bf7cc2e9d6fe51ae5494051e553f4d2
bajserowiczMikolaj <mbajserowicz@gmail.com>
2019-05-12 14:54:27 +0100

    dziala

--- src/main/java/ com/sda/algorytmy/App.java
+++ src/main/java/ com/sda/algorytmy/App.java
@@ -8,6 +8,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        int []a = {4, 1, 6, 1, 1, 4, 3, 3, 1};
+        int wynik = 0;
+        int x = 1;
+        int n = a.length;
+        for (int i =0; i<n; i++){
+            if (a[i] == x){
+                wynik = wynik +1;
+            }
+        }
+        System.out.println( x + " wystepuje " + wynik + " razy");
+
+
+
+
     }
 }
